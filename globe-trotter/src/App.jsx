7b43e3893c500bada0743957190a05e4f2a63a33
pradeep-c2005 <pradeepc.23aid@kongu.edu>
2026-01-03 13:07:56 +0530

    ExplorePage

--- globe-trotter/src/App.jsx
+++ globe-trotter/src/App.jsx
@@ -16,6 +16,7 @@ import MyTripsPage from './pages/MyTripsPage';
 import ItineraryBuilderPage from './pages/ItineraryBuilderPage';
 import ItineraryViewPage from './pages/ItineraryViewPage';
 import BudgetPage from './pages/BudgetPage';
+import ExplorePage from './pages/ExplorePage';
 import ProfilePage from './pages/ProfilePage';
 import ShareTripPage from './pages/ShareTripPage';
 
@@ -115,6 +116,12 @@ function App() {
             <Route path="/trip/:tripId/budget" element={
               <PrivateRoute>
                 <BudgetPage />
+              </PrivateRoute>
+            } />
+
+            <Route path="/explore" element={
+              <PrivateRoute>
+                <ExplorePage />
               </PrivateRoute>
             } />
             
